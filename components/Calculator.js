1e650c62586330c2879985c2fbd0932bf2e37c7f
mededaniela <mededaniela99@gmail.com>
2023-09-03 16:20:49 +0300

    Modified for fun

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,70 +1,134 @@
 import React from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import { useState } from 'react';
 
 export default function Calculator({ navigation }) {
-    const [numberone, setNumberone] = useState("");
-    const [numbertwo, setNumbertwo] = useState("");
-    const [result, setResult] = useState("");
-    const [data, setData] = useState([]);
+  const [numberone, setNumberone] = useState("");
+  const [numbertwo, setNumbertwo] = useState("");
+  const [result, setResult] = useState("");
+  const [data, setData] = useState([]);
 
-    const buttonPressedPlus = () => {
-        const calculation = `${numberone} + ${numbertwo} = ${parseFloat(numberone) + parseFloat(numbertwo)}`;
-        setData([...data, calculation]);
-        setResult(numberone + numbertwo);
-    };
+  const buttonPressedPlus = () => {
+    const calculation = `${numberone} + ${numbertwo} = ${parseFloat(numberone) + parseFloat(numbertwo)}`;
+    setData([...data, calculation]);
+    setResult(numberone + numbertwo);
+  };
 
-    const buttonPressedMinus = () => {
-        const calculation = `${numberone} - ${numbertwo} = ${parseFloat(numberone) - parseFloat(numbertwo)}`;
-        setData([...data, calculation]);
-        setResult(numberone - numbertwo);
+  const buttonPressedMinus = () => {
+    const calculation = `${numberone} - ${numbertwo} = ${parseFloat(numberone) - parseFloat(numbertwo)}`;
+    setData([...data, calculation]);
+    setResult(numberone - numbertwo);
+  }
+
+  const buttonPressedCount = () => {
+    const calculation = `${numberone} * ${numbertwo} = ${parseFloat(numberone) * parseFloat(numbertwo)}`;
+    setData([...data, calculation]);
+    setResult(numberone * numbertwo);
+  }
+
+  const buttonPressedDivide = () => {
+    const calculation = `${numberone} / ${numbertwo} = ${parseFloat(numberone) / parseFloat(numbertwo)}`;
+    setData([...data, calculation]);
+    setResult(numberone / numbertwo);
+  }
+
+  const DismissKeyboard = ({ children }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      {children}
+    </TouchableWithoutFeedback>
+  );
+
+  const wholestyle = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#fff',
+    },
+  });
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      margin: 10,
+    },
+  });
+
+  const textStyle = StyleSheet.create({
+    container: {
+      fontSize: 25,
+      padding: 10,
     }
+  });
 
-    const wholestyle = StyleSheet.create({
-        container: {
-            flex: 1,
-            margin: 150,
-        },
-    });
-
-    const styles = StyleSheet.create({
-        container: {
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-    });
-
-    const buttonstyles = StyleSheet.create({
-        container: {
-            flexDirection: 'row',
-            alignItems: 'space-between',
-            justifyContent: 'center',
-        }
+  const buttonstyles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+    },
+    space: {
+      margin: 10,
+      padding: 10,
+      fontSize: 25,
+      borderColor: 'black',
+      borderWidth: 1,
     }
-    );
+  });
 
-    return (
-        <View style={wholestyle.container}>
-            <View style={styles.container}>
-                <Text>Result: {result}</Text>
-                <TextInput style={{ width: 200, borderColor: 'gray', borderWidth: 1 }} keyboardType='numeric'
-                    onChangeText={numberone => setNumberone(numberone)} value={numberone} />
-                <TextInput style={{ width: 200, borderColor: 'gray', borderWidth: 1 }} keyboardType='numeric'
-                    onChangeText={numbertwo => setNumbertwo(numbertwo)} value={numbertwo} />
-            </View>
-            <View style={buttonstyles.container}>
-                <Button onPress={() => buttonPressedPlus()} title="+" />
-                <Button onPress={() => buttonPressedMinus()} title="-" />
-                <Button
-                    title="History"
-                    onPress={() => navigation.navigate('History', {data}, {styles})}
-                />
-            </View>
-
+  return (
+    <DismissKeyboard>
+      <View style={wholestyle.container}>
+        <View style={styles.container}>
+          <Text style={textStyle.container}>Result: {result}</Text>
+          <TextInput
+            style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+            keyboardType='numeric'
+            onChangeText={numberone => setNumberone(numberone)}
+            value={numberone}
+          />
+          <TextInput
+            style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+            keyboardType='numeric'
+            onChangeText={numbertwo => setNumbertwo(numbertwo)}
+            value={numbertwo}
+          />
         </View>
-    );
-
-
-
-};
+        <View style={buttonstyles.container}>
+          <Pressable onPress={() => buttonPressedPlus()} style={buttonstyles.space}>
+            <Text>+</Text>
+          </Pressable>
+          <Pressable onPress={() => buttonPressedMinus()} style={buttonstyles.space}>
+            <Text>-</Text>
+          </Pressable>
+          <Pressable onPress={() => buttonPressedCount()} style={buttonstyles.space}>
+            <Text>*</Text>
+          </Pressable>
+          <Pressable onPress={() => buttonPressedDivide()} style={buttonstyles.space}>
+            <Text>/</Text>
+          </Pressable>
+        </View>
+        <View>
+          <Pressable
+            style={buttonstyles.space}
+            onPress={() => navigation.navigate('History', { data }, { wholestyle })}
+          >
+            <Text>History</Text>
+          </Pressable>
+        </View>
+      </View>
+    </DismissKeyboard>
+  );
+}
--- components/History.js
+++ components/History.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
 
 export default function History({ route, navigation }) {
-    const { data } = route.params;
+  const { data } = route.params;
 
-return (
-    <View>
-    <FlatList data={data} renderItem={({item}) => <Text>{item}</Text>}
-keyExtractor={(item, index) => index.toString()} />
-<Button
-title="Calculator"
-onPress={() => navigation.navigate('Calculator')}
-/>
+  const historystyle = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      padding: 10,
+    }
+  })
+
+  return (
+    <View style={historystyle.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <Button
+        title="Calculator"
+        onPress={() => navigation.navigate('Calculator')}
+      />
     </View>
-);
-
-};
+  );
+}
